0015ebdfe5e451034f0e25f1d0ba2972e2436303
Josef Quicuma <jquicuma@c1r8s9.42luanda.com>
2024-05-15 12:29:41 +0100

    Solved All C00 LIB

---
+++ c00/includes/ft.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jquicuma <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/14 11:47:16 by jquicuma          #+#    #+#             */
+/*   Updated: 2024/05/14 11:50:24 by jquicuma         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_H
+# define FT_H
+
+# include <unistd.h>
+
+void	ft_putchar(char c);
+void    ft_print_alphabet(void);
+void    ft_print_reverse_alphabet(void);
+void    ft_print_numbers(void);
+void    ft_is_negative(int n); 
+void    ft_print_comb(void);
+void    ft_print_comb2(void);
+void    ft_putnbr(int nb);
+
+#endif
---
+++ c00/main.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jquicuma <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/14 11:50:44 by jquicuma          #+#    #+#             */
+/*   Updated: 2024/05/14 12:16:59 by jquicuma         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "includes/ft.h"
+
+int	main(void)
+{
+	
+	/*ft_print_alphabet();
+	ft_putchar('\n');
+	ft_print_reverse_alphabet();
+	ft_putchar('\n');
+	ft_print_numbers();
+	ft_putchar('\n');
+	ft_is_negative(2);
+	ft_putchar('\n');
+	ft_print_comb();
+	ft_putchar('\n');
+	ft_print_comb2();
+	ft_putnbr(-2147483648);
+	return (0);*/
+}
